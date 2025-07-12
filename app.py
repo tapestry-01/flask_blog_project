2d0a655a38ece0e2a12d6c8eb2ea9d2c8c65ba9f
tapestry-001 <worksaniket25@gmail.com>
2025-07-12 19:19:47 +0530

    Final version: hosted on PythonAnywhere with line break fix

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ def delete(post_id):
     return redirect('/admin/dashboard')
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
     with app.app_context():
         db.create_all()
     app.run(debug=True)
